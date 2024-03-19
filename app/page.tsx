41500242ad61c45dd2521a2f80e7d85f3b2d3559
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-19 19:49:44 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import {Button} from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <p>Discord</p>
-      <Button>Click me</Button>
+    <div>  <p className="text-3xl font-bold text-indigo-500">
+      Discord</p>
+      <Button >
+        Click me</Button>
     </div>
   );
 }
